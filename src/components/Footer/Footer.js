45c06569358f7899e605328ee89057bfa9a7e301
Austin Wright <awguitar86@gmail.com>
2019-05-07 07:55:19 -0600

    Updated menu to be responsive. Updated headers to be responsive. Changed the social media icon to right side of footer and made smaller.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,11 +12,13 @@ export default class Footer extends Component {
                 <div className='footer-wrap'>
                     <div className="footer-left">
                         <a className="icon-culinary-logo" href='http://iconculinary.com/' target="_blank" rel="noopener noreferrer"><img src={IconLogo} alt='reef cafe logo'/></a>
-                        <a className="facebook-logo" href='https://www.facebook.com/The-Reef-Cafe-1781006448614622/' target='_blank' rel="noopener noreferrer"><img src={facebook} alt='facebook logo'/></a>
-                        <a className="insta-logo" href='https://www.instagram.com/thereefcafe/' target='_blank' rel="noopener noreferrer"><img src={insta} alt='instagram logo'/></a>
                     </div>
 
                     <div className="footer-right">
+                        <div className="social-media">
+                            <a className="facebook-logo" href='https://www.facebook.com/The-Reef-Cafe-1781006448614622/' target='_blank' rel="noopener noreferrer"><img src={facebook} alt='facebook logo'/></a>
+                            <a className="insta-logo" href='https://www.instagram.com/thereefcafe/' target='_blank' rel="noopener noreferrer"><img src={insta} alt='instagram logo'/></a>
+                        </div>
                         <p>Website by Austin Wright</p>
                         <p>Copyright © 2018 Icon Culinary. All Rights Reserved.</p>
                     </div>
